64510772540c72e2272696c78299df6785b4e199
NadalMattia <91592803+NadalMattia@users.noreply.github.com>
2026-04-02 18:44:05 +0200

    Arduino

---
+++ tracker_sim7670g_s3/tracker_sim7670g_s3.ino
@@ -0,0 +1,10 @@
+void setup() {
+  Serial.begin(115200); // USB Serial
+  delay(1000);
+  Serial.println("T-SIM7670G-S3 Started");
+}
+
+void loop() {
+  Serial.println("TI FACCIO FUORI");
+  delay(1000);
+}
